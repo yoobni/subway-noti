5336a4d5ee3d6ec960e759c6cbba4c7407bd9acd
yoobni <yoobni1225@gmail.com>
2020-11-02 22:21:43 +0900

    Reset directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,78 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+//
+// const outputDirectory = "dist";
+//
+// module.export = {
+//     entry: "./src/client/index.js",
+//     output: {
+//         path: __dirname,
+//         filename: "./dist/bundle.js",
+//         publicPath: "/",
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: "babel-loader",
+//                 },
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     "style-loader",
+//                     "css-loader"
+//                 ]
+//             }
+//         ]
+//     },
+//     devServer: {
+//         port: 3000,
+//         open: true,
+//         proxy: {
+//             "/api": "http://localhost:8080"
+//         }
+//     },
+//     devtool: 'inline-source-map',
+//     plugins: [
+//         new CleanWebpackPlugin(),
+//         new HtmlWebpackPlugin({
+//             template: "./public/index.html"
+//         })
+//     ]
+// }
+const webpack = require("webpack");
 
-const outputDirectory = "dist";
-
-module.export = {
-    entry: "./src/client/index.js",
-    output: {
-        path: __dirname,
-        filename: "./dist/bundle.js",
-        publicPath: "/",
-    },
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.js'
+    ],
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
+                use: ['babel-loader']
             }
         ]
     },
-    devServer: {
-        port: 3000,
-        open: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
     },
-    devtool: 'inline-source-map',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
     plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        })
-    ]
-}
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
